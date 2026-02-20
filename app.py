fef8b72496b6db210123221eeb1c7eedee5cf117
mardegar13 <93151689+mardegar13@users.noreply.github.com>
2026-02-20 12:27:22 +0100

    Uploading Initial Files

---
+++ app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+from pymongo import MongoClient
+
+# 1. Configuración de la conexión
+# RECUERDA: Cambia <password> por tu contraseña real y pon tu dirección
+URL_CONEXION = "mongodb+srv://admin:botanicos@cluster0.1xe2err.mongodb.net/?appName=Cluster0"
+
+try:
+    client = MongoClient(URL_CONEXION)
+    db = client['almacen_tifos']  # Crea una base de datos llamada 'almacen_tifos'
+    coleccion = db['pinturas']    # Crea una tabla llamada 'pinturas'
+except Exception as e:
+    st.error(f"Error de conexión: {e}")
+
+st.title("🎨 Gestión de Pinturas - Tifos")
+
+# 2. Formulario para añadir pintura
+with st.form("nuevo_bote"):
+    color = st.text_input("Color del bote:")
+    litros = st.number_input("Cantidad (Litros):", min_value=0.0, step=0.5)
+    enviar = st.form_submit_button("Guardar en la Nube")
+
+    if enviar and color:
+        # Insertar en MongoDB
+        nuevo_bote = {"color": color, "litros": litros}
+        coleccion.insert_one(nuevo_bote)
+        st.success(f"¡{color} guardado correctamente!")
+
+# 3. Visualización del Inventario
+st.subheader("Inventario Actual")
+datos = list(coleccion.find())
+
+if datos:
+    for item in datos:
+        st.write(f"🖌️ **{item['color']}**: {item['litros']} Litros")
+else:
+    st.info("El almacén está vacío. ¡Añade el primer bote!")
